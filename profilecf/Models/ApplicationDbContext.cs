081381b81d19fc020d8a3e3ca95453aa2d86bcc6
arshdeepsingh <arshdeepsingh@ARSHDEEPSINA1B3>
2024-07-08 13:37:41 -0700

    Add project files.

---
+++ profilecf/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace profilecf.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection") { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ profilecf/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace profilecf.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
